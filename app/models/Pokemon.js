4c95950c937d775634b383b578866bc6bd0df2aa
RomanOlsen <romanjolsen@gmail.com>
2025-03-05 16:45:58 -0700

    better or worse?

--- app/models/Pokemon.js
+++ app/models/Pokemon.js
@@ -15,12 +15,26 @@ export class Pokemon {
 export class ActivePokemon extends Pokemon {
   constructor(d) {
     super(d)
-    this.health = d.stats[0].base_stat
-    this.healthLabel = this.health.name
 
-    this.attack = d.stats[1]
-    this.defense = d.stats[2]
-    this.speed = d.stats[5]
+    this.stat1 = d.stats[0].base_stat
+    this.label1 = d.stats[0].stat.name
+    this.stats1 = this.stat1 + ' ' + this.label1
+
+    this.stat2 = d.stats[1].base_stat
+    this.label2 = d.stats[1].stat.name
+    this.stats2 = this.stat2 + ' ' + this.label2
+
+    this.stat3 = d.stats[2].base_stat
+    this.label3 = d.stats[2].stat.name
+    this.stats3 = this.stat3 + ' ' + this.label3
+
+    this.stat4 = d.stats[5].base_stat
+    this.label4 = d.stats[5].stat.name
+    this.stats4 = this.stat4 + ' ' + this.label4
+
+    // this.attack = d.stats[1]
+    // this.defense = d.stats[2]
+    // this.speed = d.stats[5]
     this.weight = d.weight
     this.height = d.height
     this.image = d.sprites.front_default
@@ -31,12 +45,12 @@ export class ActivePokemon extends Pokemon {
           <div class="card">
             <div class="card-header">${this.name}</div>
             <img class="img-fluid" src="${this.image}" alt="picture">
-            <div class="my-1 ms-2 fs-2">${this.health} ${this.healthLabel}hp</div>
-            <div class="my-1 ms-2 fs-2">${this.attack}</div>
-            <div class="my-1 ms-2 fs-2">${this.defense}</div>
-            <div class="my-1 ms-2 fs-2">${this.speed}</div>
-            <div class="my-1 ms-2 fs-2">${this.weight}</div>
-            <div class="my-1 ms-2 fs-2">${this.height}</div>
+            <div class="my-1 ms-2 fs-2">${this.stats1}</div>
+            <div class="my-1 ms-2 fs-2">${this.stats2}</div>
+            <div class="my-1 ms-2 fs-2">${this.stats3}</div>
+            <div class="my-1 ms-2 fs-2">${this.stats4}</div>
+            <div class="my-1 ms-2 fs-2">Weight: ${this.weight}</div>
+            <div class="my-1 ms-2 fs-2">Height: ${this.height}</div>
 
           </div>
         </div>
